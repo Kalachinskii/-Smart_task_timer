505e83bade0e8bb6fd35b1680799d95cb2cf35e9
kalachinskii_vl <kalachinskii_vl@mail.ru>
2025-02-23 09:13:08 +0300

    fix: убрано изменение в LS при обновлении времени / добавлено сохранение паузы по блоку задачи|выполненые|проваленные, установка начального оставшееся времени

--- src/components/App/ui/App.jsx
+++ src/components/App/ui/App.jsx
@@ -6,7 +6,7 @@ import { TaskList } from '../../TaskList/ui/TaskList';
 
 export function App() {
   const [items, setItems] = useState([]);
-  const [btnState, setBtnState] = useState(0);
+  // const [btnState, setBtnState] = useState(0);
   
   const addItem = item => {
     setItems(oldItems => [...oldItems, {
@@ -15,6 +15,7 @@ export function App() {
       startTime: item.time,
       completed: false,
       failed: false,
+      isPlayPayse: true,
       id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
     }]);
   };
@@ -35,7 +36,8 @@ export function App() {
           ...item
         })))
       }
-  }, [btnState])
+    }, [])
+  // }, [btnState])
 
   // obj = {id: 1, state: "dailed", time: 5}
   const changeTask = (obj) => {
@@ -60,24 +62,32 @@ export function App() {
     }
   }
 
+  // obj = {newTime: 7, id: 1, state: 'time'}
+  const changeTaskTime = (obj) => {
+    const task = items.find(task => task.id === obj.id);
+    task.time = obj.newTime;
+    setItems(items.map(item => ({
+      ...item
+    })))
+  }
+
   // обновленное время с LS - решение проблемы F5
   // obj = {newTime: 7, id: 1, state: 'time'}
-  const updateTimeLS = (obj) => {
-    const data = JSON.parse(localStorage.getItem('data'));
-    data.forEach(el => {
-        if (el.id === obj.id) {
-            el.time = obj.newTime;
-        }
-      }
-    );
-    localStorage.setItem('data', JSON.stringify(data));
-    // setItems(data);
-  } 
+  // const updateTimeLS = (obj) => {
+  //   const data = JSON.parse(localStorage.getItem('data'));
+  //   data.forEach(el => {
+  //       if (el.id === obj.id) {
+  //           el.time = obj.newTime;
+  //       }
+  //     }
+  //   );
+  //   localStorage.setItem('data', JSON.stringify(data));
+  // } 
 
   return (
     <div className={styles['app']}>
       <FormItem addItem={addItem} items={items}/>
-      <TaskList tasks={items} changeCompletedTask={changeTask} changeTaskFailed={changeTask} deleteTask={deleteTask} resumeTask={changeTask} handleUpdate={updateTimeLS} setBtnState={setBtnState} btnState={btnState}/>
+      <TaskList changeIsPlayPayse={changeTask} tasks={items} changeCompletedTask={changeTask} changeTaskFailed={changeTask} deleteTask={deleteTask} resumeTask={changeTask} handleUpdate={changeTaskTime} />
     </div>
   )
 }
